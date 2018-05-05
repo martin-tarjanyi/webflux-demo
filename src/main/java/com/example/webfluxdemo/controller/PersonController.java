96b3faad967bc147ca523d963a0a206262c2f2c6
Martin <tmartin193@gmail.com>
2018-05-05 14:40:37 +0200

    Remove unnecessary null filtering

--- src/main/java/com/example/webfluxdemo/controller/PersonController.java
+++ src/main/java/com/example/webfluxdemo/controller/PersonController.java
@@ -15,7 +15,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.Objects;
 import java.util.Set;
 
 @RestController
@@ -44,7 +43,6 @@ public class PersonController
     {
         return Flux.fromIterable(numbers)
                    .flatMap(number -> reactiveRedisTemplate.opsForValue().get(number).doOnNext(log::info))
-                   .filter(Objects::nonNull)
                    .map(Integer::valueOf)
                    .flatMap(id -> mongoRepository.findById(id).doOnNext(person -> log.info(person.toString())))
                    .switchIfEmpty(Mono.error(new IllegalArgumentException("Not found person.")));
